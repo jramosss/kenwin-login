4fa1767be227d146567ac9a3a8670ea53144b1ec
jramost <jramos@seenka.com>
2023-02-15 20:23:49 -0300

    coverage utils

--- service/settings/base.py
+++ service/settings/base.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "service.middleware.ExceptionsMiddleware"
 ]
 
 ROOT_URLCONF = "service.urls"
